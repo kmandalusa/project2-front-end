3bb2b1798d0608c78aa470fa056cf298f7104dd0
Krishna Mandal <kmandalusa@gmail.com>
2021-11-09 22:20:33 -0500

    Image added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerComponent } from './customer/customer.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { RoomsComponent } from './rooms/rooms.component';
@@ -9,7 +10,8 @@ const routes: Routes = [
  { path: 'login', component: LoginComponent},
  { path: 'rooms', component: RoomsComponent},
  { path: 'registration-form', component: RegistrationFormComponent},
- { path: 'customer', component: CustomerComponent}
+ { path: 'customer', component: CustomerComponent},
+ { path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  template: `
+    <nav class="navbar">
+
+      <!-- logo -->
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img src="assets/hotel.jpg">
+        </a>
+      </div>
+    </nav>
+  `,
+  
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
